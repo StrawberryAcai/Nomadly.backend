279cfa381d796bfb9ccf09c329835dd9206e5051
eunbb <fakesilverpark@gmail.com>
2025-09-03 08:12:48 +0900

    feat :: api routing

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from fastapi import FastAPI
 from user.controller.user import router as user
 from auth.controller.auth import router as auth
 from locations.controller.distance import router as locations
+from board.controller.document import router as document
+from board.controller.actions import router as actions
 
 app = FastAPI()
 
 app.include_router(router=user)
 app.include_router(router=auth)
 app.include_router(router=locations)
+app.include_router(router=document)
+app.include_router(router=actions)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
