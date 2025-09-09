1c28394e9040cfe027afa6a72b13088e8e1d7e4c
yuuki08noah <yuuki08noah@gmail.com>
2025-09-10 01:50:56 +0900

    unstaged changes

--- user/repository/__init__.py
+++ user/repository/__init__.py
@@ -1,13 +1,21 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
-    con = psycopg2.connect(
-        host='localhost',
-        dbname='nomadly',
-        user='postgres',
-        password='postgres',
-        port=5432
-    )
+    database_url = os.getenv('DATABASE_URL')
+    if database_url:
+        con = psycopg2.connect(database_url)
+    else:
+        con = psycopg2.connect(
+            host=os.getenv('DB_HOST', 'localhost'),
+            dbname=os.getenv('DB_NAME', 'nomadly'),
+            user=os.getenv('DB_USER', 'postgres'),
+            password=os.getenv('DB_PASSWORD', 'postgres'),
+            port=int(os.getenv('DB_PORT', '5432'))
+        )
     cur = con.cursor()
     return con, cur
 
