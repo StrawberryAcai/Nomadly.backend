5d08447148c6ff0b850d15cab497d79d4df295ed
eunbb <fakesilverpark@gmail.com>
2025-09-12 03:32:42 +0900

    feat :: add functions for mypage in repository

---
+++ mypage/repository/mypage.py
@@ -0,0 +1,14 @@
+from user.repository import get_connection
+
+cur, con = get_connection()
+
+def get_plans():
+    return None
+
+
+def get_bookmark_place():
+    return None
+
+
+def get_bookmark_plans():
+    return None
--- mypage/service/mypage.py
+++ mypage/service/mypage.py
@@ -1,10 +1,13 @@
+from mypage.repository import mypage as repository
+
+
 def get_plans():
-    return None
+    return repository.get_plans()
 
 
 def get_bookmark_place():
-    return None
+    return repository.get_bookmark_place()
 
 
 def get_bookmark_plans():
-    return None
+    return repository.get_bookmark_plans()
