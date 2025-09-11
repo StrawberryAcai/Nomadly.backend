67c7f79c3ed2639056d6c53f88a2de547496acb1
eunbb <fakesilverpark@gmail.com>
2025-09-12 03:30:23 +0900

    feat :: add functions for mypage in service

--- mypage/controller/mypage.py
+++ mypage/controller/mypage.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter
 
+from mypage.service import mypage as service
+
 router = APIRouter(prefix="/api/me")
 
 @router.get("/plans")
 def get_plans():
-    return None
+    return service.get_plans()
 
 @router.get("/bookmark/place")
+def get_bookmark_place():
+    return service.get_bookmark_place()
 
 @router.get("/like/board")
 def get_bookmark_plans():
-    return None
+    return service.get_bookmark_plans()
---
+++ mypage/service/mypage.py
@@ -0,0 +1,10 @@
+def get_plans():
+    return None
+
+
+def get_bookmark_place():
+    return None
+
+
+def get_bookmark_plans():
+    return None
