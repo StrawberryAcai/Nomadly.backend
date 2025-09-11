5e8c27c8417a84623ac2ed642d12ac590c7f1fdc
eunbb <fakesilverpark@gmail.com>
2025-09-12 03:17:52 +0900

    style :: reorganize file structure and update router

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from fastapi import FastAPI
 from user.controller.user import router as user
 from auth.controller.auth import router as auth
 from locations.controller.distance import router as locations
-from mypage.bookmark.controller.bookmark import router as bookmark
-from mypage.saved.controller.saved import router as saved
+from mypage.controller.mypage import router as mypage
 
 app = FastAPI()
 
 app.include_router(router=user)
 app.include_router(router=auth)
 app.include_router(router=locations)
-app.include_router(router=bookmark)
-app.include_router(router=saved)
+app.include_router(router=mypage)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
